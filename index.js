a51422f99634f81b1e613a16a86367e04586ab71
SyntaxError404-dev <yoyoyonehal@gmail.com>
2024-10-15 19:30:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,14 @@ app.use(cors());
 
 app.get('/ques', async (req, res) => {
   try {
-    const t = req.query.t;
-    const ratio = req.query.ratio || '1:1'; // Default to '1:1' if no ratio is provided
-
+    const t = req.query.t
     const response = await axios.get(`https://sandipbaruwal.onrender.com/fluxdev?prompt=${t}&ratio=${ratio}`);
 
-    res.json(response.data); // Return the complete response data
+    res.json(response.data.answer);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error fetching data from the API' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port, () => {});
